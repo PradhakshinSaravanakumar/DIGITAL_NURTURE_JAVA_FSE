715ce765cf4feaa817f14fde2531e1509a781bb2
PRADHAKSHINA  SARAVANAKUMAR <pradhakshinasaravanakumar10@gmail.com>
2026-06-29 10:35:26 +0530

    Delete WEEK 1/MODULE 4/TDD_AND_LOGGING_FRAMEWORK/TEST_DRIVEN_DEVELOPMENT/JUNIT/EX_1-SETTING_UP directory

--- WEEK 1/MODULE 4/TDD_AND_LOGGING_FRAMEWORK/TEST_DRIVEN_DEVELOPMENT/JUNIT/EX_1-SETTING_UP/src/test/java/CalculatorTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-public class CalculatorTest{
-    @Test
-    public void testAdd(){
-        int result = 5 + 3;
-        assertEquals(8, result);
-    }
-}
