8c760ef114f6716153b9f9ed43649100f188f75e
Pradhakshina Saravanakumar <pradhakshinasaravanakumar10@gmail.com>
2026-07-03 22:27:52 +0530

    RestAPI

---
+++ WEEK 3/MODULE 7/SPRING_REST_USING_SPRING_BOOT_3/2-SPRING_REST_HANDS_ON/EX_2-SPRING_REST_COUNTRY_WEB_SERVICE/src/main/java/com/cognizant/ex_2spring_rest_country_web_service/controller/CountryController.java
@@ -0,0 +1,4 @@
+package com.cognizant.ex_2spring_rest_country_web_service.controller;
+
+public class CountryController {
+}
---
+++ WEEK 3/MODULE 7/SPRING_REST_USING_SPRING_BOOT_3/2-SPRING_REST_HANDS_ON/EX_2-SPRING_REST_COUNTRY_WEB_SERVICE/src/main/java/com/cognizant/ex_2spring_rest_country_web_service/model/Country.java
@@ -0,0 +1,4 @@
+package com.cognizant.ex_2spring_rest_country_web_service.springlearn.model;
+
+public class Country {
+}
